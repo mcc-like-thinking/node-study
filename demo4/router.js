025a515b46b4e9ef4253eae6111d3ebec0e65ac4
menghh718 <595004514@qq.com>
2019-07-04 20:37:38 +0800

    edit

--- demo4/router.js
+++ demo4/router.js
@@ -1,5 +1,5 @@
 function route(handle, pathname, response, request, postData) {
-  console.log("About to route a request for " + pathname, postData);
+  console.log("About to route a request for " + pathname);
   if (typeof handle[pathname] === 'function') {
     handle[pathname](response, request, postData);
   } else {
